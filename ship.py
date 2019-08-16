95b7cb811b37e88679d40d6463842f2596c66fb2
John Wells <molekewl2017@gmail.com>
2019-08-15 22:57:07 -0700

    stop spaceship from moving out of the screen

--- ship.py
+++ ship.py
@@ -12,7 +12,7 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         
-        # Start ach new ship at the bottom center of the screen
+        # Start new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         
@@ -24,14 +24,13 @@ class Ship():
         self.moving_left = False
     
     def update(self):
-        """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            # self.rect.centerx += 1
+        """Update the ship's position based on the movement flag."""        
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
-            # self.rect.centerx -= 1
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-        
+            
         # Update rect object from self.center
         self.rect.centerx = self.center
         
